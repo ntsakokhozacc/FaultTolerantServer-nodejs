d9686502141b074d516c9816d4d3538f968a675b
Ntsako Khoza <ntsako.khoza@dadevs.co.za>
2022-01-25 10:18:48 +0200

    ft-check if user exists before pushing to the repo

--- Desktop/Backend-movie/src/users/controller.js
+++ Desktop/Backend-movie/src/users/controller.js
@@ -1,4 +1,3 @@
-const res = require('express/lib/response');
 const pool = require('../../db');
 const queries = require('./queries')
 //const getUser = "SELECT * from users ORDER BY id ASC";
@@ -18,6 +17,32 @@ const getUsers = (req, res) => {
     console.log("debuggging");
 };
 
+const getUserById=(req,res) =>{
+    const id =parseInt(req.params.id);
+    pool.query(queries.getUserById,[id],(error, results)=>{
+        if(error) throw error;
+        res.status(200).json(results.rows);
+    } );
+};
+
+
+const addUser = (req,res) => {
+    const {name,email,age,dob} = req.body;
+
+    //check if email exists
+    pool.query(queries.checkEmailExists, [email], (error, results) => {
+        
+        if (results.rows.length){
+            res.send("email already exists");
+        }
+        
+    });
+    
+    
+};
+
 module.exports ={
     getUsers,
+    getUserById,
+    addUser,
 };
--- Desktop/Backend-movie/src/users/queries.js
+++ Desktop/Backend-movie/src/users/queries.js
@@ -1,6 +1,10 @@
 
 const getUsers = "SELECT * from users ORDER BY id ASC";
+const getUserById = "select * from users where id =$1"
+const checkEmailExists = "select * from users where email= $1"
 
 module.exports = {
     getUsers,
+    getUserById,
+    checkEmailExists
 };
